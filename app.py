446543860d2167b64b784def6b7a18174ab37263
HP_desktop <lsi429@naver.com>
2026-04-11 09:25:31 +0900

    iframe3

--- app.py
+++ app.py
@@ -220,11 +220,11 @@ def authenticate_docs(credentials: HTTPBasicCredentials = Depends(security)):
     return credentials.username
 
 @app.get("/docs", include_in_schema=False)
-async def get_documentation(username: str = Depends(authenticate_docs)):
+async def get_documentation():
     return get_swagger_ui_html(openapi_url=app.openapi_url, title=app.title + " - Swagger UI")
 
 @app.get("/redoc", include_in_schema=False)
-async def get_redoc_documentation(username: str = Depends(authenticate_docs)):
+async def get_redoc_documentation():
     return get_redoc_html(openapi_url=app.openapi_url, title=app.title + " - ReDoc")
 
 
